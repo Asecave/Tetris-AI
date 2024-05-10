6366fbc1c9593d50a3b5ac107319f3ae43535283
Asecave <39377667+Asecave@users.noreply.github.com>
2024-05-10 15:28:24 +0200

    tetris working

--- src/main/java/com/asecave/Main.java
+++ src/main/java/com/asecave/Main.java
@@ -1,28 +1,156 @@
 package com.asecave;
 
-class Main {
-	
-	public static void main(String[] args) {
-		
-		Tetris tetris = new Tetris();
-		
-		for (int i = 0; i < 10; i++) {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+class Main extends JPanel implements KeyListener {
+
+	JFrame frame;
+
+	final int blockSize = 50;
+
+	Tetris tetris = new Tetris();
+
+	public Main() {
+
+		frame = new JFrame("AI Tetris");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setPreferredSize(new Dimension(blockSize * 22, blockSize * 22));
+		frame.add(this);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.addKeyListener(this);
+		frame.setVisible(true);
+
+		while (true) {
 			tetris.gravity();
-			
-			byte[] board = tetris.getBoard();
-			
-			for (int j = 0; j < board.length; j++) {
-				System.out.print(String.format("%8s", Integer.toBinaryString(board[j] & 0xFF)).replace(' ', '0'));
-				if (j % 3 == 2) System.out.println();
-			}
-			System.out.println();
-			
+			frame.repaint();
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-    }
-	
+
+//		for (int i = 0; i < 100; i++) {
+//			tetris.gravity();
+//			tetris.roatateLeft();
+//
+//			int[] board = tetris.getBoard();
+//
+//			System.out.println(Arrays.toString(tetris.getNextPieces()));
+//			for (int j = 0; j < board.length; j++) {
+//				System.out.println(String.format("%10s", Integer.toBinaryString(board[j] & 0xFFFF)).replace(' ', '0')
+//						.replaceAll("0", "."));
+//			}
+//			System.out.println();
+//
+//			try {
+//				Thread.sleep(200);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+
+		g2d.setColor(Color.DARK_GRAY);
+		g2d.fillRect(0, 0, frame.getWidth(), frame.getHeight());
+		
+		g2d.setColor(Color.GREEN);
+		if (tetris.getHoldPiece() != -1) {
+			int[][] shape = tetris.getPieceShape(tetris.getHoldPiece());
+			for (int x = 0; x < shape.length; x++) {
+				for (int y = 0; y < shape[0].length; y++) {
+					if (shape[y][x] != 1)
+						continue;
+					g2d.fill3DRect(x * blockSize + blockSize / 2, y * blockSize - blockSize / 2, blockSize, blockSize, true);
+				}
+			}
+		}
+		
+		int[] nextPieces = tetris.getNextPieces();
+		for(int i = 0; i < nextPieces.length; i++) {
+			int[][] shape = tetris.getPieceShape(nextPieces[i]);
+			for (int x = 0; x < shape.length; x++) {
+				for (int y = 0; y < shape[0].length; y++) {
+					if (shape[y][x] != 1)
+						continue;
+					g2d.fill3DRect((x + 17) * blockSize + blockSize / 2, y * blockSize - blockSize / 2 + (i * 3 * blockSize), blockSize, blockSize, true);
+				}
+			}
+		}
+
+		g2d.translate(blockSize * 6, blockSize);
+
+		g2d.setColor(new Color(Color.DARK_GRAY.getRGB()).darker());
+		g2d.fillRect(-blockSize, -blockSize, blockSize * 12, blockSize * 22);
+		int[] board = tetris.getBoard();
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < 10; j++) {
+				int current = 0b1000000000 >> j;
+				if ((current & board[i]) > 0) {
+					g2d.setColor(Color.GREEN);
+					g2d.fill3DRect(j * blockSize, i * blockSize, blockSize, blockSize, true);
+				} else {
+					g2d.setColor(Color.DARK_GRAY);
+					g2d.fill3DRect(j * blockSize, i * blockSize, blockSize, blockSize, true);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_ESCAPE:
+			System.exit(0);
+		case KeyEvent.VK_A:
+			tetris.moveLeft();
+			break;
+		case KeyEvent.VK_D:
+			tetris.moveRight();
+			break;
+		case KeyEvent.VK_LEFT:
+			tetris.rotateLeft();
+			break;
+		case KeyEvent.VK_RIGHT:
+			tetris.rotateRight();
+			break;
+		case KeyEvent.VK_UP:
+			tetris.hardDrop();
+			break;
+		case KeyEvent.VK_SHIFT:
+			tetris.hold();
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+
 }
--- src/main/java/com/asecave/Tetris.java
+++ src/main/java/com/asecave/Tetris.java
@@ -1,202 +1,422 @@
 package com.asecave;
 
+import java.util.Iterator;
+
 public class Tetris {
 
-	private byte[] board;
-	
-	private byte currentPiece;
-	private byte rotation;
-	
-	private byte[] bag, nextBag;
-	
-	private byte posX, posY;
-	
-	private static final byte[][][] TETROMINOS = {
-		{
-			{0, 0, 0, 0}, // I
-			{0, 0, 0, 0},
-			{1, 1, 1, 1},
-			{0, 0, 0, 0},
-		},{
-			{0, 1, 0, 0}, // I
-			{0, 1, 0, 0},
-			{0, 1, 0, 0},
-			{0, 1, 0, 0},
-		},{
-			{0, 0, 0, 0}, // I
-			{1, 1, 1, 1},
-			{0, 0, 0, 0},
-			{0, 0, 0, 0},
-		},{
-			{0, 0, 1, 0}, // I
-			{0, 0, 1, 0},
-			{0, 0, 1, 0},
-			{0, 0, 1, 0},
-		},{
-			{0, 0, 0, 0}, // J
-			{1, 0, 0, 0},
-			{1, 1, 1, 0},
-			{0, 0, 0, 0},
-		},{
-			{0, 0, 0, 0}, // J
-			{0, 1, 1, 0},
-			{0, 1, 0, 0},
-			{0, 1, 0, 0},
-		},{
-			{0, 0, 0, 0}, // J
-			{0, 0, 0, 0},
-			{1, 1, 1, 0},
-			{0, 0, 1, 0},
-		},{
-			{0, 0, 0, 0}, // J
-			{0, 1, 0, 0},
-			{0, 1, 0, 0},
-			{1, 1, 0, 0},
-		},{
-			{0, 0, 0, 0}, // L
-			{0, 0, 1, 0},
-			{1, 1, 1, 0},
-			{0, 0, 0, 0},
-		},{
-			{0, 0, 0, 0}, // L
-			{0, 1, 0, 0},
-			{0, 1, 0, 0},
-			{0, 1, 1, 0},
-		},{
-			{0, 0, 0, 0}, // L
-			{0, 0, 0, 0},
-			{1, 1, 1, 0},
-			{1, 0, 0, 0},
-		},{
-			{0, 0, 0, 0}, // L
-			{1, 1, 0, 0},
-			{0, 1, 0, 0},
-			{0, 1, 0, 0},
-		},{
-			{0, 0, 0, 0}, // O
-			{0, 1, 1, 0},
-			{0, 1, 1, 0},
-			{0, 0, 0, 0},
-		},{
-			{0, 0, 0, 0}, // O
-			{0, 1, 1, 0},
-			{0, 1, 1, 0},
-			{0, 0, 0, 0},
-		},{
-			{0, 0, 0, 0}, // O
-			{0, 1, 1, 0},
-			{0, 1, 1, 0},
-			{0, 0, 0, 0},
-		},{
-			{0, 0, 0, 0}, // O
-			{0, 1, 1, 0},
-			{0, 1, 1, 0},
-			{0, 0, 0, 0},
-		},{
-			{0, 0, 0, 0}, // S
-			{0, 1, 1, 0},
-			{1, 1, 0, 0},
-			{0, 0, 0, 0},
-		},{
-			{0, 0, 0, 0}, // S
-			{0, 1, 0, 0},
-			{0, 1, 1, 0},
-			{0, 0, 1, 0},
-		},{
-			{0, 0, 0, 0}, // S
-			{0, 0, 0, 0},
-			{0, 1, 1, 0},
-			{1, 1, 0, 0},
-		},{
-			{0, 0, 0, 0}, // S
-			{1, 0, 0, 0},
-			{1, 1, 0, 0},
-			{0, 1, 0, 0},
-		},{
-			{0, 0, 0, 0}, // T
-			{0, 1, 0, 0},
-			{1, 1, 1, 0},
-			{0, 0, 0, 0},
-		},{
-			{0, 0, 0, 0}, // T
-			{0, 1, 0, 0},
-			{0, 1, 1, 0},
-			{0, 1, 0, 0},
-		},{
-			{0, 0, 0, 0}, // T
-			{0, 0, 0, 0},
-			{1, 1, 1, 0},
-			{0, 1, 0, 0},
-		},{
-			{0, 0, 0, 0}, // T
-			{0, 1, 0, 0},
-			{1, 1, 0, 0},
-			{0, 1, 0, 0},
-		},{
-			{0, 0, 0, 0}, // Z
-			{1, 1, 0, 0},
-			{0, 1, 1, 0},
-			{0, 0, 0, 0},
-		},{
-			{0, 0, 0, 0}, // Z
-			{0, 0, 1, 0},
-			{0, 1, 1, 0},
-			{0, 1, 0, 0},
-		},{
-			{0, 0, 0, 0}, // Z
-			{0, 0, 0, 0},
-			{1, 1, 0, 0},
-			{0, 1, 1, 0},
-		},{
-			{0, 0, 0, 0}, // Z
-			{0, 1, 0, 0},
-			{1, 1, 0, 0},
-			{1, 0, 0, 0},
-		},
+	private int[] board;
+
+	private int currentPiece;
+	private int rotation;
+
+	private int[] bag, nextBag;
+
+	private int posX, posY;
+	private int currentLockDownPass;
+	private int hold;
+
+	private static final int START_X = 3;
+	private static final int START_Y = -4;
+	private static final int LOCK_DOWN_PASSES = 4;
+
+	// @formatter:off
+	private static final int[][][] TETROMINOS = { 
+			{ 
+				{ 0, 0, 0, 0 }, // I
+				{ 0, 0, 0, 0 }, 
+				{ 1, 1, 1, 1 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 1, 0, 0 }, // I
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // I
+				{ 1, 1, 1, 1 }, 
+				{ 0, 0, 0, 0 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 1, 0 }, // I
+				{ 0, 0, 1, 0 }, 
+				{ 0, 0, 1, 0 }, 
+				{ 0, 0, 1, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // J
+				{ 1, 0, 0, 0 }, 
+				{ 1, 1, 1, 0 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // J
+				{ 0, 1, 1, 0 }, 
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // J
+				{ 0, 0, 0, 0 }, 
+				{ 1, 1, 1, 0 }, 
+				{ 0, 0, 1, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // J
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+				{ 1, 1, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // L
+				{ 0, 0, 1, 0 }, 
+				{ 1, 1, 1, 0 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // L
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 1, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // L
+				{ 0, 0, 0, 0 }, 
+				{ 1, 1, 1, 0 }, 
+				{ 1, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // L
+				{ 1, 1, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // O
+				{ 0, 1, 1, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // O
+				{ 0, 1, 1, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // O
+				{ 0, 1, 1, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // O
+				{ 0, 1, 1, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // S
+				{ 0, 1, 1, 0 }, 
+				{ 1, 1, 0, 0 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // S
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 0, 1, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // S
+				{ 0, 0, 0, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 1, 1, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // S
+				{ 1, 0, 0, 0 }, 
+				{ 1, 1, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // T
+				{ 0, 1, 0, 0 }, 
+				{ 1, 1, 1, 0 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // T
+				{ 0, 1, 0, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 1, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // T
+				{ 0, 0, 0, 0 }, 
+				{ 1, 1, 1, 0 }, 
+				{ 0, 1, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // T
+				{ 0, 1, 0, 0 }, 
+				{ 1, 1, 0, 0 }, 
+				{ 0, 1, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // Z
+				{ 1, 1, 0, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 0, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // Z
+				{ 0, 0, 1, 0 }, 
+				{ 0, 1, 1, 0 }, 
+				{ 0, 1, 0, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // Z
+				{ 0, 0, 0, 0 }, 
+				{ 1, 1, 0, 0 }, 
+				{ 0, 1, 1, 0 }, 
+			},{ 
+				{ 0, 0, 0, 0 }, // Z
+				{ 0, 1, 0, 0 }, 
+				{ 1, 1, 0, 0 }, 
+				{ 1, 0, 0, 0 }, 
+			}, 
+		};
+
+	private final static int[][] JLSTZ_KICK_TABLE = {
+			{0, 0, -1, 0, -1, +1, 0, -2, -1, -2},
+			{0, 0, +1, 0, +1, -1, 0, +2, +1, +2},
+			{0, 0, +1, 0, +1, -1, 0, +2, +1, +2},
+			{0, 0, -1, 0, -1, +1, 0, -2, -1, -2},
+			{0, 0, +1, 0, +1, +1, 0, -2, +1, -2},
+			{0, 0, -1, 0, -1, -1, 0, +2, -1, +2},
+			{0, 0, -1, 0, -1, -1, 0, +2, -1, +2},
+			{0, 0, +1, 0, +1, +1, 0, -2, +1, -2},
 	};
 	
+	private final static int[][] I_KICK_TABLE = {
+			{0, 0, -2, 0, +1, 0, -2, -1, +1, +2},
+			{0, 0, +2, 0, -1, 0, +2, +1, -1, -2},
+			{0, 0, -1, 0, +2, 0, -1, +2, +2, -1},
+			{0, 0, +1, 0, -2, 0, +1, -2, -2, +1},
+			{0, 0, +2, 0, -1, 0, +2, +1, -1, -2},
+			{0, 0, -2, 0, +1, 0, -2, -1, +1, +2},
+			{0, 0, +1, 0, -2, 0, +1, -2, -2, +1},
+			{0, 0, -1, 0, +2, 0, -1, +2, +2, -1},
+	};
+	// @formatter:on
+
 	public Tetris() {
-		
-		board = new byte[3 * 10];
+
+		board = new int[20];
 		bag = generateBag();
 		nextBag = generateBag();
+
+		rotation = 0;
+		posX = START_X;
+		posY = START_Y;
+		hold = -1;
 	}
-	
+
 	public void gravity() {
-		
+
+		posY++;
+
+		if (hasCollision()) {
+			posY--;
+			if (currentLockDownPass > LOCK_DOWN_PASSES) {
+				placePiece();
+				currentLockDownPass = 0;
+			} else {
+				currentLockDownPass++;
+			}
+		} else {
+			currentLockDownPass = 0;
+		}
+	}
+
+	public void moveLeft() {
+
+		posX--;
+
+		if (hasCollision()) {
+			posX++;
+		}
+	}
+
+	public void moveRight() {
+
+		posX++;
+
+		if (hasCollision()) {
+			posX--;
+		}
+	}
+
+	public int[] getNextPieces() {
+		int[] next = new int[5];
+		int p = 0;
+		for (int i = currentPiece + 1; i < currentPiece + 1 + next.length; i++) {
+			if (i >= bag.length) {
+				next[p] = nextBag[i - bag.length];
+			} else {
+				next[p] = bag[i];
+			}
+			p++;
+		}
+		return next;
+	}
+
+	public void rotateLeft() {
+		int previousRotation = rotation;
+		int previousX = posX;
+		int previousY = posY;
+		rotation--;
+		if (rotation < 0)
+			rotation += 4;
+		final int[] kickTable;
+		if (bag[currentPiece] == 0)
+			kickTable = I_KICK_TABLE[rotation * 2 + 1];
+		else
+			kickTable = JLSTZ_KICK_TABLE[rotation * 2 + 1];
+		for (int i = 0; i < kickTable.length; i += 2) {
+			posX += kickTable[i + 0];
+			posY -= kickTable[i + 1];
+			if (hasCollision()) {
+				posX = previousX;
+				posY = previousY;
+			} else {
+				currentLockDownPass = 0;
+				return;
+			}
+		}
+		rotation = previousRotation;
+	}
+	
+	public void rotateRight() {
+		int previousRotation = rotation;
+		int previousX = posX;
+		int previousY = posY;
+		rotation++;
+		if (rotation > 3)
+			rotation -= 4;
+		final int[] kickTable;
+		if (bag[currentPiece] == 0)
+			kickTable = I_KICK_TABLE[rotation * 2];
+		else
+			kickTable = JLSTZ_KICK_TABLE[rotation * 2];
+		for (int i = 0; i < kickTable.length; i += 2) {
+			posX += kickTable[i + 0];
+			posY -= kickTable[i + 1];
+			if (hasCollision()) {
+				posX = previousX;
+				posY = previousY;
+			} else {
+				currentLockDownPass = 0;
+				return;
+			}
+		}
+		rotation = previousRotation;
+	}
+	
+	public void hold() {
+		if (hold == -1) {
+			hold = bag[currentPiece];
+			nextPiece();
+		} else {
+			int tmp = hold;
+			hold = bag[currentPiece];
+			bag[currentPiece] = tmp;
+			posX = START_X;
+			posY = START_Y;
+			rotation = 0;
+		}
 		
 	}
 	
+	public int getHoldPiece() {
+		return hold;
+	}
+
+	private boolean hasCollision() {
+		int[][] piece = getCurrentPiece();
+		for (int x = 0; x < piece.length; x++) {
+			for (int y = posY; y < posY + piece[0].length; y++) {
+				if (y < 0)
+					continue;
+				if (piece[y - posY][x] == 0)
+					continue;
+				if (posX + x < 0 || posX + x > 9)
+					return true;
+				if (y >= 20 || (board[y] & (0b1000000000 >> posX + x)) > 0)
+					return true;
+			}
+		}
+		return false;
+	}
+
 	private void placePiece() {
-		
-		byte piece = (byte) (bag[currentPiece] * 4 + rotation);
-		
-		if (currentPiece == 8) {
+		placePieceOnBoard(board);
+		clearLines();
+		nextPiece();
+	}
+	
+	private int clearLines() {
+		int linesCleared = 0;
+		for (int i = 0; i < board.length; i++) {
+			if ((board[i] ^ 0b1111111111) == 0) {
+				for (int j = i; j > 1; j--) {
+					board[j] = board[j - 1];
+				}
+				linesCleared++;
+			}
+		}
+		return linesCleared;
+	}
+	
+	private void nextPiece() {
+		currentPiece++;
+
+		if (currentPiece == 7) {
 			bag = nextBag;
 			nextBag = generateBag();
 			currentPiece = 0;
 		}
+
+		posX = START_X;
+		posY = START_Y;
+		rotation = 0;
 	}
-	
+
 	public void hardDrop() {
-		// TODO dropping
+		while (!hasCollision()) {
+			posY++;
+		}
+		posY--;
+		placePiece();
+		currentLockDownPass = 0;
 	}
-	
-	private byte[] generateBag() {
-		byte[] newBag = new byte[7];
-		for (byte i = 0; i < newBag.length; i++) {
+
+	private int[] generateBag() {
+		int[] newBag = new int[7];
+		for (int i = 0; i < newBag.length; i++) {
 			newBag[i] = i;
 		}
-		for (byte i = 0; i < newBag.length; i++) {
-			byte random = (byte) (Math.random() * newBag.length);
-			byte tmp = newBag[i];
+		for (int i = 0; i < newBag.length; i++) {
+			int random = (int) (Math.random() * newBag.length);
+			int tmp = newBag[i];
 			newBag[i] = newBag[random];
 			newBag[random] = tmp;
 		}
 		return newBag;
 	}
+
+	public int[] getBoard() {
+		int[] b = board.clone();
+		placePieceOnBoard(b);
+		return b;
+	}
+
+	private void placePieceOnBoard(int[] board) {
+		int[][] piece = getCurrentPiece();
+
+		for (int x = 0; x < piece.length; x++) {
+			for (int y = posY; y < posY + piece[0].length; y++) {
+				if (y < 0)
+					continue;
+				if (piece[y - posY][x] == 0)
+					continue;
+				board[y] = board[y] | (0b1000000000 >> posX + x);
+			}
+		}
+	}
+
+	private int[][] getCurrentPiece() {
+		return TETROMINOS[bag[currentPiece] * 4 + rotation];
+	}
 	
-	public byte[] getBoard() {
-		return board.clone();
+	public int[][] getPieceShape(int piece) {
+		return TETROMINOS[piece * 4];
 	}
 }
